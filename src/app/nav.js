e67f44bc1201fc96ccd11f93c12e6418221f6a14
AnibeA <anibeojonugwa.alhassan@student.griffith.ie>
2024-04-22 11:16:43 +0100

    addition of Navbar

---
+++ src/app/nav.js
@@ -0,0 +1,22 @@
+
+
+export default function Navigation() {
+    return (
+      <nav>
+        <ul>
+          <li>
+              <a>Home</a>
+          </li>
+          <li>
+              <a>About</a>
+          </li>
+          <li>
+              <a>Projects</a>
+          </li>
+          <li>
+              <a>Contact</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
